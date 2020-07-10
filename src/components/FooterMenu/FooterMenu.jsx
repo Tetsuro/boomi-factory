f4030a7b0f084fd8c1c3302d088dcf49d3ab0f25
Tetsuro <tetsuro.takara@gmail.com>
2020-07-09 22:01:45 -0400

    Clear styles, start adding basic layout

---
+++ src/components/FooterMenu/FooterMenu.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import styles from 'FooterMenu.scss';
+
+function FooterMenu() {
+  return <nav className='FooterMenu'>Footer menu</nav>;
+}
+
+export default FooterMenu;
