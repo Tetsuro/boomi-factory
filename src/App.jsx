d2b574fcad1fb9c8d841ace7789b501ffa699db2
Tetchi Takara <tetchibook@Tetchis-MacBook.local>
2020-12-12 10:15:09 +1300

    Add markup and layer SVGs for colour swapping

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,29 @@ import Header from "./components/Header";
 import Preview from "./components/Preview";
 import PrintButton from "./components/PrintButton";
 import Sheet from "./components/Sheet";
-
+import {colors} from "./data/colors.json";
 
 import styles from "./App.module.scss";
 
 function App() {
   const [designSheetIsOpen, setDesignSheetIsOpen] = useState(false);
   const [customizeSheetIsOpen, setCustomizeSheetIsOpen] = useState(false);
-
+  const [activeColor, setActiveColor] = useState(colors[0]);
 
   const handleSheetClose = () => {
     setDesignSheetIsOpen(false);
     setCustomizeSheetIsOpen(false);
   };
 
+
+  // TODO: Sheet should have "open" prop to toggle.
   const customizeSheetMarkup = customizeSheetIsOpen ? (
     <Sheet title="Customize a Boomi" onClose={handleSheetClose}>
       Customize
     </Sheet>
   ) : null;
 
+  // TODO: Sheet should have "open" prop to toggle.
   const designSheetMarkup = designSheetIsOpen ? (
     <Sheet title="Select a design" onClose={handleSheetClose}>
       Design
@@ -51,7 +54,7 @@ function App() {
           label: "Select a design"
         }
       ]} />
-      <Preview />
+      <Preview activeColor={activeColor} />
       <PrintButton />
       {customizeSheetMarkup}
       {designSheetMarkup}
