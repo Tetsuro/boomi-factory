62128c07765564e9ab68825159084f60680a442f
tetchi <tetchi@hey.com>
2020-12-24 14:18:40 +1300

    Add Transition to MobileMenu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import { useTranslation } from 'react-i18next';
+import {CSSTransition} from 'react-transition-group';
+import {classNames} from '../../utilities/classNames';
 
 import styles from "./MobileMenu.module.scss";
 
-function MobileMenu({ open }) {
+function MobileMenu({ open=false }) {
   const {t, i18n} = useTranslation();
 
-  return open ? (
-    <nav className={styles.MobileMenu}>
-      <h2>{t('menu.menu')}</h2>
-      <ul>
-        <li>
-          <a href="#">{t('menu.whats_a_boomi')}</a>
-        </li>
-        <li>
-          <a href="#">{t('menu.donate')}</a>
-        </li>
-      </ul>
-      <ul>
-        <li><button onClick={() => i18n.changeLanguage('en')}>🇬🇧 English</button></li>
-        <li><button onClick={() => i18n.changeLanguage('fr')}>🇫🇷 Français</button></li>
-        <li><button onClick={() => i18n.changeLanguage('jp')}>🇯🇵 日本語</button></li>
-      </ul>
-    </nav>
-  ) : null;
+  const transitionClassNames = { 
+    enter: classNames(styles.MobileMenu),
+    enterDone: classNames(styles.MobileMenu, styles['MobileMenu-enter-done']),
+    exitActive: classNames(styles.MobileMenu),
+    exitDone: classNames(styles.MobileMenu),
+  };
+
+  return (
+    <CSSTransition in={open} timeout={{enter: 0, exit: 200}} classNames={transitionClassNames} unmountOnExit>
+      <nav>
+        <h2>{t('menu.menu')}</h2>
+        <ul>
+          <li>
+            <a href="#">{t('menu.whats_a_boomi')}</a>
+          </li>
+          {/* <li>
+            <a href="#">{t('menu.donate')}</a>
+          </li> */}
+        </ul>
+        <ul>
+          <li><button onClick={() => i18n.changeLanguage('en')}>🇬🇧 English</button></li>
+          <li><button onClick={() => i18n.changeLanguage('fr')}>🇫🇷 Français</button></li>
+          <li><button onClick={() => i18n.changeLanguage('jp')}>🇯🇵 日本語</button></li>
+        </ul>
+      </nav>
+    </CSSTransition>
+  );
 }
 
 export default MobileMenu;
