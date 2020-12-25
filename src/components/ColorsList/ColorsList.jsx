7df07126fdcdfe5392dd2b67d5c08dfd28a6cfe3
tetchi <tetchi@hey.com>
2020-12-25 14:28:44 +1300

    Add styles to ColorsList

--- src/components/ColorsList/ColorsList.jsx
+++ src/components/ColorsList/ColorsList.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import styles from './ColorsList.module.scss';
 import {colors} from "../../data/colors.json";
+import {classNames} from '../../utilities/classNames';
 
-function ColorsList({onSwatchClick}) {
+function ColorsList({activeColor, onSwatchClick}) {
   function handleClick(color) {
     onSwatchClick(color);
   }
+
+  console.log(activeColor);
+
+  
   // TODO: Better key for li?
-  const listMarkup = colors.map((color, index) => (
-    <li key={index}>
-      <button className={styles.Button} onClick={() => handleClick(color)}>
-        <div className={styles.SwatchWrapper}>
-          <span className={styles.Swatch} style={{'backgroundColor': color.primary}}></span>
-          <span className={styles.Swatch} style={{'backgroundColor': color.secondary}}></span>
-        </div>
-        <div className={styles.Label}>{color.name}</div>
-      </button>
-    </li>
-  ));
+  const listMarkup = colors.map((color, index) => {
+    const isActive = color === activeColor;
+    const buttonClassNames = classNames(styles.Button, isActive && styles.active);
+   return (
+      <li key={index}>
+        <button className={buttonClassNames} onClick={() => handleClick(color)}>
+          <div className={styles.SwatchWrapper}>
+            <span className={styles.Swatch} style={{'backgroundColor': color.primary}}></span>
+            <span className={styles.Swatch} style={{'backgroundColor': color.secondary}}></span>
+          </div>
+          <div className={styles.Label}>{color.name}</div>
+        </button>
+      </li>
+    )}
+  );
 
   return (
     <ul className={styles.ColorsList}>
