6079c0f6a0f32e44184c395711f4654b35b566c2
Tetsuro <tetsuro.takara@gmail.com>
2021-02-25 15:45:34 -0500

    Add Blink preview svg

--- src/components/EyesList/eyesMap.js
+++ src/components/EyesList/eyesMap.js
@@ -2,7 +2,7 @@ import { ReactComponent as Original } from './svg/original.svg';
 import { ReactComponent as OriginalPreview } from './svg/original-preview.svg';
 
 import { ReactComponent as Blink } from './svg/blink.svg';
-// import { ReactComponent as BlinkPreview } from './svg/blink-preview.svg';
+import { ReactComponent as BlinkPreview } from './svg/blink-preview.svg';
 
 import { ReactComponent as Meh } from './svg/meh.svg';
 import { ReactComponent as MehPreview } from './svg/meh-preview.svg';
@@ -22,7 +22,7 @@ export const eyesArray = [
   {
     name: 'Blink',
     svg: Blink,
-    previewSvg: OriginalPreview,
+    previewSvg: BlinkPreview,
   },
   {
     name: 'Meh',
