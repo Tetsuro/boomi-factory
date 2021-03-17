d145ab87aa906456197949f2ba87c98b23c8b571
Tetsuro <tetsuro.takara@gmail.com>
2021-03-17 15:43:01 -0400

    Remove commented code

--- src/components/ButtonGroup/ButtonGroup.jsx
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -5,7 +5,6 @@ import Button from '../Button';
 function ButtonGroup({ buttons }) {
   const buttonsMarkup = buttons.map((button, index) => (
     <li key={index}>
-      {/* <button onClick={button.onClick}>{button.label}</button> */}
       <Button onClick={button.onClick}>{button.label}</Button>
     </li>
   ));
