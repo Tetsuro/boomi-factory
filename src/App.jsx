eb8d2b934b2f2ae4f7574ac74c78dfd968527f9c
Tetsuro <tetsuro.takara@gmail.com>
2021-03-28 21:26:53 -0400

    Remove Button import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import Button from './components/Button';
 import ButtonGroup from './components/ButtonGroup';
 import Header from './components/Header';
 import Preview from './components/Preview';
