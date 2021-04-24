0f157026e61bb5c18a5993fe32a7fef53b109ece
Tetsuro <tetsuro.takara@gmail.com>
2021-04-23 22:09:13 -0400

    Add Sharingan

--- src/components/EyesList/eyesMap.js
+++ src/components/EyesList/eyesMap.js
@@ -30,6 +30,10 @@ import { ReactComponent as Rage } from './svg/rage.svg';
 import { ReactComponent as RagePreview } from './svg/rage-preview.svg';
 import { ReactComponent as RageBlueprint } from './svg/rage-blueprint.svg';
 
+import { ReactComponent as Sharingan } from './svg/sharingan.svg';
+import { ReactComponent as SharinganPreview } from './svg/sharingan-preview.svg';
+import { ReactComponent as SharinganBlueprint } from './svg/sharingan-blueprint.svg';
+
 export const eyesMap = {
   original: Original,
   blink: Blink,
@@ -50,16 +54,16 @@ export const eyesArray = [
     blueprintSvg: BlinkBlueprint,
   },
   {
-    name: 'Ouch!',
-    svg: Dead,
-    previewSvg: DeadPreview,
-    blueprintSvg: DeadBlueprint,
-  },
-  {
     name: 'Dizzy',
     svg: Dizzy,
     previewSvg: DizzyPreview,
     blueprintSvg: DizzyBlueprint,
+  },
+  {
+    name: 'Grrr',
+    svg: Rage,
+    previewSvg: RagePreview,
+    blueprintSvg: RageBlueprint,
   },
   {
     name: 'Hearts',
@@ -80,9 +84,15 @@ export const eyesArray = [
     blueprintSvg: OldTimeyBlueprint,
   },
   {
-    name: 'Grrr',
-    svg: Rage,
-    previewSvg: RagePreview,
-    blueprintSvg: RageBlueprint,
+    name: 'Ouch!',
+    svg: Dead,
+    previewSvg: DeadPreview,
+    blueprintSvg: DeadBlueprint,
+  },
+  {
+    name: 'Sharingan',
+    svg: Sharingan,
+    previewSvg: SharinganPreview,
+    blueprintSvg: SharinganBlueprint,
   },
 ];
